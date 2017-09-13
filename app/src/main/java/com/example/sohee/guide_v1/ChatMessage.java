834d1155d6b41aa5a6ec8095c723bcb6ff25a6b2
Sohee <dbsthgml7121@naver.com>
2017-09-13 17:52:27 +0900

    9/13 changes2

---
+++ app/src/main/java/com/example/sohee/guide_v1/ChatMessage.java
@@ -0,0 +1,39 @@
+package com.example.sohee.guide_v1;
+
+import java.util.Random;
+
+public class ChatMessage {
+
+
+        public boolean left;
+        public String message;
+
+        public ChatMessage(boolean left, String message) {
+            super();
+            this.left = left;
+            this.message = message;
+        }
+
+
+
+    public String body, sender, receiver, senderName;
+    public String Date, Time;
+    public String msgid;
+    public boolean isMine;// Did I send the message.
+
+    public ChatMessage(String Sender, String Receiver, String messageString,
+                       String ID, boolean isMINE) {
+        body = messageString;
+        isMine = isMINE;
+        sender = Sender;
+        msgid = ID;
+        receiver = Receiver;
+        senderName = sender;
+    }
+
+    public void setMsgID() {
+
+        msgid += "-" + String.format("%02d", new Random().nextInt(100));
+        ;
+    }
+}
